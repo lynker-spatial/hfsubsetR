d4ada1392c1aae327d19fb40001959be3367ffef
arashmodrad <arashmodaresirad@u.boisestate.edu>
2024-05-21 12:18:51 -0600

    updated thetthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(hydrofabric)
+library(hfsubsetR)
 
-test_check("hydrofabric")
+test_check("hfsubsetR")
