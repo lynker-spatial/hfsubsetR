421f80d858c78b517347b12d6133800911d911f2
program-- <justin@justinsingh.me>
2024-05-26 13:13:54 -0700

    refactor: separate findOrigin into S3 methods

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+.onLoad <- function(...) {
+  .S3method("findOriginQuery", "default")
+  .S3method("findOriginQuery", "hf_id")
+  .S3method("findOriginQuery", "comid")
+  .S3method("findOriginQuery", "hl_uri")
+  .S3method("findOriginQuery", "poi_id")
+  .S3method("findOriginQuery", "nldi_feature")
+  .S3method("findOriginQuery", "xy")
+}
