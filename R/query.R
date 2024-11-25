2a9dd95c135ebfc963831b74997bb07aae650441
program-- <justin@justinsingh.me>
2024-11-25 12:55:45 -0800

    docs: update inline documentation

--- R/query.R
+++ R/query.R
@@ -1,15 +1,15 @@
-.assert_query <- function(query) {
-  if (!inherits(query, "hfsubset_query")) {
-    stop("Argument `query` is not an `hfsubset_query` object.", call. = FALSE)
-  }
-}
-
 #' Initialize a new hfsubset query
+#' @returns A `hfsubset_query` object
 #' @export
 query <- function() {
   structure(list(), class = "hfsubset_query")
 }
 
+#' Set the identifier of a query
+#' @param query A `hfsubset_query` object
+#' @param identifier Identifier value
+#' @param type Identifier type
+#' @returns `query` with the identifier included
 #' @export
 query_set_id <- function(query, identifier, type = c("id", "comid", "hl_uri", "poi_id", "nldi_feature", "xy")) {
   .assert_query(query)
@@ -21,6 +21,10 @@ query_set_id <- function(query, identifier, type = c("id", "comid", "hl_uri", "p
   query
 }
 
+#' Set the layers of a query
+#' @inheritParams query_set_id
+#' @param layers A `character` vector of layer names
+#' @returns `query` with the layers included
 #' @export
 query_set_layers <- function(query, layers) {
   .assert_query(query)
@@ -35,6 +39,11 @@ query_set_layers <- function(query, layers) {
   query
 }
 
+#' Set the source of a query
+#' @inheritParams query_set_id
+#' @param src A `hfsubset_query_source` object
+#' @returns `query` with the source included
+#' @seealso query_source_arrow query_source_sf
 #' @export
 query_set_source <- function(query, src) {
   .assert_query(query)
@@ -49,6 +58,11 @@ query_set_source <- function(query, src) {
   query
 }
 
+#' Set the sink of a query
+#' @inheritParams query_set_id
+#' @param sink A character path to sink
+#' @param overwrite If TRUE, then if the sink exists, it should be overwritten
+#' @returns `query` with the sink included
 #' @export
 query_set_sink <- function(query, sink, overwrite = FALSE) {
   .assert_query(query)
@@ -60,22 +74,37 @@ query_set_sink <- function(query, sink, overwrite = FALSE) {
   query
 }
 
+#' Accessor to get the ID of a query
+#' @inheritParams query_set_id
 #' @keywords internal
 query_get_id <- function(query) {
   query$identifier
 }
 
+#' Accessor to get the layers of a query
+#' @inheritParams query_set_id
 #' @keywords internal
 query_get_layers <- function(query) {
   query$layers
 }
 
+#' Accessor to get the source of a query
+#' @inheritParams query_set_id
 #' @keywords internal
 query_get_source <- function(query) {
   query$source
 }
 
+#' Accessor to get the sink of a query
+#' @inheritParams query_set_id
 #' @keywords internal
 query_get_sink <- function(query) {
   query$sink
+}
+
+#' @keywords internal
+.assert_query <- function(query) {
+  if (!inherits(query, "hfsubset_query")) {
+    stop("Argument `query` is not an `hfsubset_query` object.", call. = FALSE)
+  }
 }
